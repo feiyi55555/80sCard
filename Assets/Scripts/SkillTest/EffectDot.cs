da070902e7555ec08c7d476fd65ddefa68f5dcf1
wuxudong <457336590@qq.com>
2018-08-05 17:46:52 +0800

    add effectdot and add skillTest Scenes.(Prepering Skill System.)

---
+++ Assets/Scripts/SkillTest/EffectDot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectDot : MonoBehaviour {
+
+    private Dictionary<string, Transform> m_DotDic = new Dictionary<string, Transform>();
+    public List<Transform> DotList = new List<Transform>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < DotList.Count; i++)
+        {
+            var DotListi = DotList[i];
+            if (!m_DotDic.ContainsKey(DotListi.name))
+            {
+                // 后续战斗中会根据这些点dot索引出位置。
+                m_DotDic.Add(DotListi.name, DotListi);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 根据这些点dotName索引出位置。
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public Transform GetEffectDot(string name)
+    {
+        return m_DotDic.ContainsKey(name) ? m_DotDic[name] : null;
+    }
+}
